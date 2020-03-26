fe169b7525444595b21d949ba121277600dcfc9b
yashaleo <yashaleo@me.com>
2020-03-26 12:05:54 -0500

    added backend routes, added react client folder

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
-const express = require('express'); 
+const express = require('express');
 const connectDB = require('./config/db');
 
-const app =  express();
+const app = express();
 
 //Connect Database
 connectDB();
 
-app.get('/', (req, res) => res.json({msg: 'Welcome to the ContactKeeper API....'}));
+//Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) =>
+  res.json({ msg: 'Welcome to the ContactKeeper API....' })
+);
 
 //Define Routes
 app.use('/api/users', require('./routes/users'));
@@ -16,4 +21,3 @@ app.use('/api/contacts', require('./routes/contacts'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
